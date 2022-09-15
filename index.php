7d9c77a63cd71e21e0c30e1044f8c6a1880e7559
bijoy2000 <111699440+bijoy2000@users.noreply.github.com>
2022-09-15 13:33:08 +0600

    partices php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mamurjor PHP</title>
+</head>
+<body>
+    <?php 
+       $parson_name = "Bijoy";
+       $age = 22;
+       $number = 20.25;
+       echo "Hello PHP" . "<br>";
+       echo "My Name is : " . $parson_name . "<br>" . "I am " . $age . " year old" . "<br>";
+       print $parson_name . "<br>";
+       var_dump($parson_name);
+       echo "<br>";
+       var_dump($age);
+       echo "<br>";
+       var_dump($number);
+
+
+    ?>
+</body>
+</html>
